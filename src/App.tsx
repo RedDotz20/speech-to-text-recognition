5e68f258d456e83c7a6fe550c9a42e02b01e7f39
RedDotz20 <carlos_tabangay@outlook.com>
2023-05-10 16:19:12 +0800

    feat: speech recognition

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+import SpeechRecognitionApp from './components/SpeechRecog';
+
+import './App.css';
+
+function App() {
+	return (
+		<Fragment>
+			<SpeechRecognitionApp />
+		</Fragment>
+	);
+}
+
+export default App;
